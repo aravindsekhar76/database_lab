618ecd94835f468c8f3c2ce7eab8642105d18c7f
Aravind Sekhar <115882981+aravindsekhar76@users.noreply.github.com>
2023-09-22 22:11:12 +0530

    Update mon.py

--- DB prep/mon.py
+++ DB prep/mon.py
@@ -7,7 +7,7 @@
 # for i in collection.find({"course":"MCA"}):
 #     print(i)
 
-import pymongo 
+import pymong
 conn= pymongo.MongoClient("mongodb://localhost:27017/")
 
 # print(conn.list_database_names())
